b8269e6846341a3d13d0c39bdb5e5b2786146694
NRskog <143582096+NRskog@users.noreply.github.com>
2024-10-09 20:38:33 +0200

    Update expression.h

--- Lab3/expression.h
+++ Lab3/expression.h
@@ -7,10 +7,7 @@
 #include <stack>
 #include <memory>
 
-// Komplettering: Ni läcker Node-klassen i det publika gränssnittet.        KLAR
-// Komplettering: Ett Expression-objekt behöver endast bestå av en pekare till den översta noden. KLAR
-// Komplettering: hjälpfunktioner ska ligga privat i klassen.           KLAR
-// Komplettering: Parameteröverföring av sammansatta datatyper ska ske som konstant referens om möjligt.    KLAR
+
 
 
 class Expression
